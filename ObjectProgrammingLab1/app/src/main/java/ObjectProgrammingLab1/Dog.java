ee44285b4fa49dc4ad3d01a887a37bcccf621b09
unknown <inf154030@lab-dba-3.cs.put.poznan.pl>
2022-10-15 11:39:33 +0200

    Adding project to repository

---
+++ ObjectProgrammingLab1/app/src/main/java/ObjectProgrammingLab1/Dog.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ObjectProgrammingLab1;
+
+/**
+ *
+ * @author inf154030
+ */
+public class Dog {
+ 
+    int age;
+    String name;
+    String sound;
+    String favouriteToy;
+    
+    public Dog(int age, String name, String sound, String favouriteToy){
+        
+        this.age = age;
+        this.name = name;
+        this.sound = sound;
+        this.favouriteToy = favouriteToy;
+    }
+}
---
+++ ObjectProgrammingLab1/app/src/main/java/ObjectProgrammingLab1/Owner.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ObjectProgrammingLab1;
+
+/**
+ *
+ * @author inf154030
+ */
+public class Owner {
+    
+    int age;
+    String name;
+    String favouritePet;
+    
+    public Owner(int age, String name, String favouritePet){
+        
+        this.age = age;
+        this.name = name;
+        this.favouritePet = favouritePet;
+    }
+}
