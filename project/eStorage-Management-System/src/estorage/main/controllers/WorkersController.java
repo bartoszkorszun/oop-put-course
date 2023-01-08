95462949743a51986ebbe534a9ba0a8582345a53
Bartosz Korszun <bartoszkorszun@gmail.com>
2023-01-08 01:18:21 +0100

    Organising imports

--- project/eStorage-Management-System/src/estorage/main/controllers/WorkersController.java
+++ project/eStorage-Management-System/src/estorage/main/controllers/WorkersController.java
@@ -21,7 +21,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
